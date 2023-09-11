c03d2b87d35ef8d81c439a53ba7def2a4e418ea1
ifranco07import <i.francomtz07@gmail.com>
2023-09-11 15:46:31 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,32 @@
+//Operadores Matematicos
+// + - * / % **
+// += -= *= /= %= 
+console.log( 10 + 10);
+console.log( 10 - 10);
+console.log( 10 / 10);
+console.log( 10 * 10);
+console.log( 10 % 10);
+console.log( 10 ** 10);
+
+let num = 15;
+console.log( num += 5); //num = num + 
+console.log( num -= 5);
+console.log( num *= 5);
+console.log( num /= 5);
+console.log( num %= 5);
+
+//Operadores booleanos
+console.log( 1 >2);
+console.log( 1 <2);
+console.log( 1 > 2);
+console.log( 1 <= 2);
+console.log( 1 !==2);
+console.log( 1 === 2);
+
+//Operadores Lógicos
+//and &&
+console.log(true && true);
+//or ||
+console.log(true || false);
+//not !
+console.log(!true);
