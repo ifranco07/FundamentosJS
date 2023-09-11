1905a0540fc2a8265b7617704166153b2ee5642c
ifranco07import <i.francomtz07@gmail.com>
2023-09-11 15:58:53 -0600

    05 Metodos y Propiedades strings

---
+++ strings2.js
@@ -0,0 +1,9 @@
+//Métodos y propiedades Strings
+const text = 'EsTe eS Un TeXtO NicE';
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(2));
+console.log(text.indexOf('T'));
+console.log(text.startsWith('E'));
+console.log(text.repeat(10));
