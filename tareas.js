9b4a9754e13a00192a0ed594be75042af60c024c
ifranco07import <i.francomtz07@gmail.com>
2023-09-20 15:42:25 -0600

    13 Lista de Tareas

---
+++ tareas.js
@@ -0,0 +1,22 @@
+const inputTarea = document.getElementById('inputTarea');
+const inputTiempo = document.getElementById('tiempo');
+const listaTareas= document.querySelector('#tareas ul');
+
+const tarea=[];
+
+function addTarea(){
+    const dato= inputTarea.value;
+    const tiempo= inputTiempo.value;
+    if(dato){
+        tarea.push({tarea: dato, time: tiempo});
+        inputTarea.value= '';
+        inputTiempo.value= '';
+        showTareas();
+    }
+   
+}
+
+function showTareas(){
+    const tareasLi=tarea.map(tarea => `<li> ${tarea.tarea} - ${tarea.time}</li>`);
+    listaTareas.innerHTML = tareasLi.join('');
+}
