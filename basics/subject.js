d026ee440763217206e6c549411e636a21138cab
ifranco07import <i.francomtz07@gmail.com>
2023-09-25 15:00:38 -0600

    14 DOM colores rgb

---
+++ basics/subject.js
@@ -0,0 +1,17 @@
+//Objetos en javascripts
+/**
+ objeto = {
+    key: value,
+    key: value
+ }
+ objeto.key
+ objeto['key']
+ */
+const subject = {
+    name: 'Aplicaciones Web',
+    cuatri: 3,
+    profesor: 'Francisco',
+    'classroom': 'Lab Redes I',
+    'total hours': 70,
+    weekHours: 5
+};
